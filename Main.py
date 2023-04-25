94671ef7c7217c292bc88917e4c2a211580ed144
SleepWalker <suravienkov@mail.ru>
2023-04-25 04:52:58 +0300

    done

--- Main.py
+++ Main.py
@@ -3,17 +3,17 @@ import random
 import string
 import os
 import sqlite3
+import ast
 from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Cipher import AES
 
-def name(name):
-    return sg.Text(name, size=(16, 1), justification='r',pad=(0,0))
-
 layout = [
     [sg.Text('Введите мастер-ключ:'), sg.InputText(key='-PASS-')],
-    [sg.Button('Сгенерировать мастер-ключ'), sg.Checkbox('Цифры', k='-NUMBER-'), sg.Checkbox('Спец. символы', k='-SPEC-'), sg.Checkbox('Разные регистры', k='-REGISTER-'),
-    name('Длина мастер-ключа:'), sg.Combo(['6','8','10','12','16'], default_value='12', s=(15,22), enable_events=True, readonly=True, k='-LENGTH-')],
+    [sg.Button('Сгенерировать мастер-ключ'), sg.Checkbox('Цифры', k='-NUMBER-'),
+    sg.Checkbox('Спец. символы', k='-SPEC-'), sg.Checkbox('Разные регистры', k='-REGISTER-'),
+    sg.Text('Длина мастер-ключа:', size=(16, 1), justification='r',pad=(0,0)),
+    sg.Combo(['6','8','10','12','16'], default_value='12', s=(15,22), enable_events=True, readonly=True, k='-LENGTH-')],
     [sg.Button('Зашифровать'), sg.Button('Расшифровать')],
     [sg.Text(key='-TEXT-')]
 ]
@@ -27,13 +27,13 @@ while True:
         break
     
     elif event == 'Зашифровать':
-        input_pass = values['-PASS-']
+        password = values['-PASS-']
         
         conn = sqlite3.connect('password.db')
         cursor = conn.cursor()
         conn.commit()
                 
-        def PassShifrate(password):
+        def PassEncrypt(password):
             password = password.encode("utf-8")
             salt = get_random_bytes(32)
             cursor.execute(f"UPDATE password SET salt = '{list(salt)}';"); conn.commit()
@@ -54,7 +54,7 @@ while True:
                 if dbInit == "OK":
                     cursor.execute("DELETE FROM password;"); conn.commit()
                     cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
-                    PassShifrate(input_pass)
+                    PassEncrypt(password)
                     return
             
             table = sg.popup("Создание необходимых таблиц!",)
@@ -70,7 +70,7 @@ while True:
                     check_db_exists()
 
         def EmptyPassCheck():
-            if input_pass.strip() == '':
+            if password.strip() == '':
                 new_text = 'Вы не ввели мастер-ключ!'
                 window['-TEXT-'].update(new_text)
             else:
@@ -79,6 +79,24 @@ while True:
 
                 
         EmptyPassCheck()
+        
+    elif event == 'Расшифровать':
+        
+        def passDecrypt():
+            passwordDecrypt = password.encode('utf-8')
+            cursor.execute("SELECT salt FROM password;"); saltDecrypt = bytes(ast.literal_eval(str(cursor.fetchall())[3:-4]))
+            keyDecrypt = PBKDF2(passwordDecrypt, saltDecrypt, dkLen=32)
+            cursor.execute("SELECT iv FROM password"); ivDecrypt = bytes(ast.literal_eval(str(cursor.fetchall())[3:-4]))
+            cursor.execute("SELECT ciphered_bytes FROM password"); DecryptedBytes = bytes(ast.literal_eval(str(cursor.fetchall())[3:-4]))
+            cipher_decrypt = AES.new(keyDecrypt, AES.MODE_CFB, iv=ivDecrypt)
+            deciphered_bytes = cipher_decrypt.decrypt(DecryptedBytes)
+            decrypted_data = deciphered_bytes.decode('utf-8')
+            if decrypted_data == values['-PASS-']:
+                sg.popup("Мастер-ключ успешно расшифрован")
+            else:
+                sg.popup("Ошибка! Скорее всего Вы ввели неправильный мастер-ключ!")
+            
+        passDecrypt()
         
     elif event == 'Сгенерировать мастер-ключ':
         
