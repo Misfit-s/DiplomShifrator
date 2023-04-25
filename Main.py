4f1a1ae44eafdce4e34d3d836b11e2705bee1b2e
SleepWalker <suravienkov@mail.ru>
2023-04-25 03:28:44 +0300

    first commit

---
+++ Main.py
@@ -0,0 +1,97 @@
+import PySimpleGUI as sg
+import random
+import string
+import os
+import sqlite3
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+
+def name(name):
+    return sg.Text(name, size=(16, 1), justification='r',pad=(0,0))
+
+layout = [
+    [sg.Text('Введите мастер-ключ:'), sg.InputText(key='-PASS-')],
+    [sg.Button('Сгенерировать мастер-ключ'), sg.Checkbox('Цифры', k='-NUMBER-'), sg.Checkbox('Спец. символы', k='-SPEC-'), sg.Checkbox('Разные регистры', k='-REGISTER-'),
+    name('Длина мастер-ключа:'), sg.Combo(['6','8','10','12','16'], default_value='12', s=(15,22), enable_events=True, readonly=True, k='-LENGTH-')],
+    [sg.Button('Войти')],
+    [sg.Text(key='-TEXT-')]
+]
+
+window = sg.Window('Шифрование слова', layout)
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WINDOW_CLOSED:
+        break
+    
+    elif event == 'Войти':
+        input_pass = values['-PASS-']
+        
+        conn = sqlite3.connect('password.db')
+        cursor = conn.cursor()
+        conn.commit()
+        sg.popup("База данных Password была успешно создана/подключена!")
+
+        def check_db_exists():
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='password'")
+            table_exists = cursor.fetchall()
+            if table_exists:
+                sg.popup("База данных инициализированна!")
+                cursor.execute("DELETE FROM password;"); conn.commit()
+                cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
+                return
+            sg.popup("Создание необходимых таблиц!")
+            createTable = """CREATE TABLE password (
+                          salt TEXT,
+                          iv TEXT,
+                          ciphered_bytes TEXT)"""
+            cursor.execute(createTable); conn.commit()
+            cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
+
+        check_db_exists()
+
+        def EmptyPassCheck():
+            if input_pass.strip() == '':
+                new_text = 'Вы не ввели мастер-ключ!'
+                window['-TEXT-'].update(new_text)
+                
+        EmptyPassCheck()
+                
+        def PassShifrate(password):
+            password = password.encode("utf-8")
+            salt = get_random_bytes(32)
+            cursor.execute(f"UPDATE password SET salt = '{list(salt)}';"); conn.commit()
+            key = PBKDF2(password, salt, dkLen=32)
+            cipher_encrypt = AES.new(key, AES.MODE_CFB)
+            ciphered_bytes = cipher_encrypt.encrypt(password)
+            cursor.execute(f"UPDATE password SET ciphered_bytes = '{list(ciphered_bytes)}';"); conn.commit()
+            iv = cipher_encrypt.iv
+            cursor.execute(f"UPDATE password SET iv = '{list(iv)}';"); conn.commit()
+            sg.popup("Ваш мастер-ключ успешно зашифрован!")
+            
+        PassShifrate(input_pass)
+        
+    elif event == 'Сгенерировать мастер-ключ':
+        
+        def PassGen():
+            pass_length = int(values['-LENGTH-'])
+            pswd = []
+            
+            for i in range(pass_length):
+                pswd.append(random.choice(string.ascii_lowercase))
+                if values['-NUMBER-']:
+                    pswd.append(random.choice(string.digits))
+                if values['-SPEC-']:
+                    pswd.append(random.choice(string.punctuation))
+                if values['-REGISTER-']:
+                    pswd.append(random.choice(string.ascii_uppercase))
+                    
+            random.shuffle(pswd)
+            
+            window['-PASS-'].update("".join(map(str,pswd))[:pass_length])
+            
+        PassGen()
+
+window.close()
