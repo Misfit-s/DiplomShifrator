9af81a15e66129351bb80e089943c063b152922c
SleepWalker <suravienkov@mail.ru>
2023-04-25 04:00:24 +0300

    second commit

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ layout = [
     [sg.Text('Введите мастер-ключ:'), sg.InputText(key='-PASS-')],
     [sg.Button('Сгенерировать мастер-ключ'), sg.Checkbox('Цифры', k='-NUMBER-'), sg.Checkbox('Спец. символы', k='-SPEC-'), sg.Checkbox('Разные регистры', k='-REGISTER-'),
     name('Длина мастер-ключа:'), sg.Combo(['6','8','10','12','16'], default_value='12', s=(15,22), enable_events=True, readonly=True, k='-LENGTH-')],
-    [sg.Button('Войти')],
+    [sg.Button('Зашифровать'), sg.Button('Расшифровать')],
     [sg.Text(key='-TEXT-')]
 ]
 
@@ -26,38 +26,12 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
     
-    elif event == 'Войти':
+    elif event == 'Зашифровать':
         input_pass = values['-PASS-']
         
         conn = sqlite3.connect('password.db')
         cursor = conn.cursor()
         conn.commit()
-        sg.popup("База данных Password была успешно создана/подключена!")
-
-        def check_db_exists():
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='password'")
-            table_exists = cursor.fetchall()
-            if table_exists:
-                sg.popup("База данных инициализированна!")
-                cursor.execute("DELETE FROM password;"); conn.commit()
-                cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
-                return
-            sg.popup("Создание необходимых таблиц!")
-            createTable = """CREATE TABLE password (
-                          salt TEXT,
-                          iv TEXT,
-                          ciphered_bytes TEXT)"""
-            cursor.execute(createTable); conn.commit()
-            cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
-
-        check_db_exists()
-
-        def EmptyPassCheck():
-            if input_pass.strip() == '':
-                new_text = 'Вы не ввели мастер-ключ!'
-                window['-TEXT-'].update(new_text)
-                
-        EmptyPassCheck()
                 
         def PassShifrate(password):
             password = password.encode("utf-8")
@@ -70,8 +44,41 @@ while True:
             iv = cipher_encrypt.iv
             cursor.execute(f"UPDATE password SET iv = '{list(iv)}';"); conn.commit()
             sg.popup("Ваш мастер-ключ успешно зашифрован!")
+        
+        def check_db_exists():
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='password'")
+            table_exists = cursor.fetchall()
             
-        PassShifrate(input_pass)
+            if table_exists:
+                dbInit = sg.popup("База данных инициализированна!")
+                if dbInit == "OK":
+                    cursor.execute("DELETE FROM password;"); conn.commit()
+                    cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
+                    PassShifrate(input_pass)
+                    return
+            
+            table = sg.popup("Создание необходимых таблиц!",)
+            if table == "OK":
+                createTable = """CREATE TABLE password (
+                              salt TEXT,
+                              iv TEXT,
+                              ciphered_bytes TEXT)"""
+                cursor.execute(createTable); conn.commit()
+                cursor.execute("INSERT INTO password (salt, iv, ciphered_bytes) VALUES (NULL, NULL, NULL);"); conn.commit()
+                dbDone = sg.popup("База данных создана и настроена!")
+                if dbDone == "OK":
+                    check_db_exists()
+
+        def EmptyPassCheck():
+            if input_pass.strip() == '':
+                new_text = 'Вы не ввели мастер-ключ!'
+                window['-TEXT-'].update(new_text)
+            else:
+                window['-TEXT-'].update('')
+                check_db_exists()
+
+                
+        EmptyPassCheck()
         
     elif event == 'Сгенерировать мастер-ключ':
         
